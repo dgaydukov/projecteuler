8f757a6fbf55b972ee98b6f81b5f7b7f1d3dbae3
Dmitriy Gaydukov <gaydukov89@gmail.com>
2019-07-10 11:20:51 +0800

    add task 5

---
+++ tasks/5.cpp
@@ -0,0 +1,45 @@
+/*
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+ */
+
+#include <iostream>
+#include <ctime>
+#include <math.h>
+
+bool IsDivisibleUpTo(int num, int upToNum)
+{
+    for (int i = upToNum; i > 1; i--)
+    {
+        if (num % i != 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int run(int upToNum)
+{
+    int num = upToNum;
+    while (true)
+    {
+        if (IsDivisibleUpTo(num, upToNum))
+        {
+            return num;
+        }
+        num++;
+    }
+}
+
+int main()
+{
+    std::clock_t begin = std::clock();
+    int result = run(22);
+    std::cout << "Result: " << result << std::endl;
+    std::clock_t end = std::clock();
+    std::cout << "Time taken: " << (end - begin) / CLOCKS_PER_SEC << std::endl;
+    return 0;
+}
